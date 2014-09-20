de206c4d796234ccf793457240309a8e21eaff99
qJake <jake@burgy.me>
2014-09-20 17:44:16 -0500

    Added missing untracked files

---
+++ SharpHue/Config/UpdateInfo.cs
@@ -0,0 +1,40 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharpHue.Config
+{
+    /// <summary>
+    /// Contains information about a bridge software update.
+    /// </summary>
+    [JsonObject]
+    public class UpdateInfo
+    {
+        /// <summary>
+        /// Gets the update status. This is 0 if there are no updates available.
+        /// </summary>
+        [JsonProperty("updatestate")]
+        public int UpdateStatus { get; private set; }
+
+        /// <summary>
+        /// Gets the URL of the update.
+        /// </summary>
+        [JsonProperty("url")]
+        public string Url { get; private set; }
+
+        /// <summary>
+        /// Gets the message text to display to the user about the update.
+        /// </summary>
+        [JsonProperty("text")]
+        public string Message { get; private set; }
+
+        /// <summary>
+        /// Gets whether or not a notification should be shown to the user that an update is available.
+        /// </summary>
+        [JsonProperty("notify")]
+        public bool ShouldNotify { get; private set; }
+    }
+}
---
+++ SharpHue/Config/WhitelistItem.cs
@@ -0,0 +1,43 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharpHue.Config
+{
+    /// <summary>
+    /// Contains information about a single whitelist entry (or user).
+    /// </summary>
+    [JsonObject]
+    public class WhitelistItem
+    {
+        /// <summary>
+        /// Gets the application name / ID of this entry.
+        /// </summary>
+        [JsonProperty("name")]
+        public string ApplicationID { get; private set; }
+
+        /// <summary>
+        /// Gets when this user ID was last used.
+        /// </summary>
+        [JsonProperty("last use date")]
+        public DateTime LastUsed { get; private set; }
+
+        /// <summary>
+        /// Gets when this user ID was first created on the bridge.
+        /// </summary>
+        [JsonProperty("create date")]
+        public DateTime Created { get; private set; }
+
+        /// <summary>
+        /// Gets a string representation of this whitelist entry.
+        /// </summary>
+        /// <returns>A string representing this whitelist entry.</returns>
+        public override string ToString()
+        {
+            return "Whitelist: " + ApplicationID;
+        }
+    }
+}
